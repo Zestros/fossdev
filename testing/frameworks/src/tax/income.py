56f4bac0dce819d923862943ea029171d0681859
Zestros <dardanel2006dan@mail.ru>
2026-03-05 15:06:48 +0800

    added automatical test by unittest

--- testing/frameworks/src/tax/income.py
+++ testing/frameworks/src/tax/income.py
@@ -1,6 +1,2 @@
 def calculate_tax(income):
-    if income < 0:
-        raise ValueError(
-            "Could not hav negative income"
-        )
-    return int(income * 0.15 * 100) / 100
+    return int(income * 0.13 * 100) / 100
