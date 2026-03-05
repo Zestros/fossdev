96e9d67cfbc6dcbdab568d671492c6ce63eb9f89
Zestros <dardanel2006dan@mail.ru>
2026-03-05 14:17:55 +0800

    added tests-should-check-exception principle

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -70,6 +70,13 @@ def test_tax_calculator():
     assert calculate_tax(2.34) == 0.35
     print("test unbugged tax calculator passed")
 
+def test_negatie_income():
+    try:
+        calculate_tax(-100):
+        print("test negative income falied")
+    except ValueError as e:
+        print("test negative income passed")
+
 if __name__ == '__main__':
     test_addition()
     #test_addition_with_bug()
@@ -78,4 +85,5 @@ if __name__ == '__main__':
     test_addition_commutative()
     test_tax_calculator_pesticide()
     test_tax_calculator()
+    test_negatie_income
     #test_addition_overkill()
