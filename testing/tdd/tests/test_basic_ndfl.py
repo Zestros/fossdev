8eb2502ae7fa2a97bd7fabf606c1ab8dbdb414f6
Zestros <dardanel2006dan@mail.ru>
2026-03-10 14:37:08 +0800

    Added tests for basic ndlf colculation

---
+++ testing/tdd/tests/test_basic_ndfl.py
@@ -0,0 +1,18 @@
+#TODO make test to obey principles
+
+import ndfl import calculate_ndfl
+
+def test_ndfl_tier_1_basic():
+    assert calculate_ndfl(2_000_000) == 260_000
+
+def test_ndfl_tier_2_basic():
+    assert calculate_ndfl(4_000_000) == 552_000
+
+def test_ndfl_tier_3_basic():
+    assert calculate_ndfl(10_000_000) == 1_602_000
+
+def test_ndfl_tier_4_basic():
+    assert calculate_ndfl(30_000_000) == 5_402_000
+
+def test_ndfl_tier_5_basic():
+    assert calculate_ndfl(60_000_000) == 11_602_000
