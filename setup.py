9bd73f86e199292b960d2cbd0b3a058c4cdeb784
Zestros <dardanel2006dan@mail.ru>
2026-03-31 20:27:12 +0800

    Added discription README in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="text-utils-lite",
     version="0.1.0",
     description="Simple text utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="My name and lastname",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
