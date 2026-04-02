4e63d13a50144486df21315a49c12dc0772d6ff3
Zestros <dardanel2006dan@mail.ru>
2026-04-02 13:07:31 +0800

    Added check float quantity

--- docs_domain/src/sales.py
+++ docs_domain/src/sales.py
@@ -2,14 +2,19 @@
 
 
 def _parse_record(line: str):
-    p = line.strip().split(",")
+    sale = line.strip().split(",")
     if len(p) != 4:  # according specc. each is defined by 4 field
         return None  # return nothing
 
-    product_name = p[0] 
-    category = p[1] 
-    unit_price = float(p[2])
-    quantity = int(p[3])
+    product_name = sale[0] 
+    category = sale[1] 
+    try:
+        unit_price = float(sale[2])
+        quantity = int(sale[3])
+        if int(sale[3]) != float(sale[3]):
+            return None
+    except ValueError:
+        return None
 
     return {"n": product_name, "c": category, "a": unit_price, "q": quantity}  # make dict
 
