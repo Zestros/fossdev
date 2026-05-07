f34b88ce151b948c118e8773b8eceb24e442f74c
Zestros <dardanel2006dan@mail.ru>
2026-05-07 14:48:57 +0800

    Fixed buuugs in setting.py

--- order_service/src/app/settings.py
+++ order_service/src/app/settings.py
@@ -1,11 +1,12 @@
 # order_service/app/settings.py
-import os
 from dataclasses import dataclass
+import os
 
 
 @dataclass(frozen=True)
 class Settings:
     product_service_url: str
+    discount_service_url: str
     database_url: str
 
 
@@ -13,14 +14,14 @@ def get_settings() -> Settings:
     return Settings(
         product_service_url=os.getenv(
             "PRODUCT_SERVICE_URL",
-            "http://127.0.0.1:8001",
+            "http://product-service:8000",
         ),
         discount_service_url=os.getenv(
             "DISCOUNT_SERVICE_URL",
-            "http://127.0.0.1:8002",
+            "http://discount-service:8000",
         ),
         database_url=os.getenv(
             "DATABASE_URL",
-            "postgresql://app:app@127.0.0.1:5432/orders",
+            "postgresql://app:app@postgres:5432/orders",
         ),
     )
